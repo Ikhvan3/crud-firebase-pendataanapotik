a11db9568352aef84d054ec3d757f388d1aa54b5
Ikhvan <ikhvankanaya123@gmail.com>
2024-07-06 18:00:54 +0700

    koneksi mysql dan firebase

--- lib/model/modelobat.dart
+++ lib/model/modelobat.dart
@@ -1,22 +1,23 @@
-class Obat {
-  String id;
-  String kodeObat;
-  String namaObat;
+// class Barang {
+//   String id;
+//   String kodeBarang;
+//   String namaBarang;
 
-  Obat({required this.id, required this.kodeObat, required this.namaObat});
+//   Barang(
+//       {required this.id, required this.kodeBarang, required this.namaBarang});
 
-  Map<String, dynamic> toMap() {
-    return {
-      'kode_obat': kodeObat,
-      'nama_obat': namaObat,
-    };
-  }
+//   Map<String, dynamic> toMap() {
+//     return {
+//       'kode_barang': kodeBarang,
+//       'nama_barang': namaBarang,
+//     };
+//   }
 
-  static Obat fromMap(String id, Map<String, dynamic> map) {
-    return Obat(
-      id: id,
-      kodeObat: map['kode_obat'],
-      namaObat: map['nama_obat'],
-    );
-  }
-}
+//   static Barang fromMap(String id, Map<String, dynamic> map) {
+//     return Barang(
+//       id: id,
+//       kodeBarang: map['kode_barang'],
+//       namaBarang: map['nama_barang'],
+//     );
+//   }
+// }
